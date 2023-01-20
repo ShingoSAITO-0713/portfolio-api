bb63db62ba64fe307946d50d595ce1d1b27f8719
ShingoSAITO-0713 <g2122026@fun.ac.jp>
2023-01-20 19:04:44 +0900

    著者のdbの追加

--- routers/put.py
+++ routers/put.py
@@ -8,9 +8,22 @@ with open('settings.json') as f:
     ENV_KEYS = json.load(f)
     PROJECT_KEY = ENV_KEYS['PROJECT_KEY']
     DB_THESIS = ENV_KEYS['DB_THESIS']
+    DB_AUTHORS_MASTER = ENV_KEYS['DB_AUTHORS_MASTER']
+    DB_AUTHORS = ENV_KEYS['DB_AUTHORS']
 
 DETA = Deta(PROJECT_KEY)
-DB = DETA.Base(DB_THESIS)
+THESIS = DETA.Base(DB_THESIS)
+AUTHORS_MASTER = DETA.Base(DB_AUTHORS_MASTER)
+# {
+#     'key': str,
+#     'author_name': str
+# }
+AUTHORS = DETA.Base(DB_AUTHORS)
+# {
+#     'key': str
+#     'thesis_id': str
+#     'author_id': str
+# }
 
 router = APIRouter(
     prefix='/put',
@@ -30,14 +43,36 @@ class ThesisData(BaseModel):
 
 @router.put('/')
 async def put_data(thesis_data: ThesisData):
-    thesis = DB.put(
+    author_names = thesis_data.authors
+    thesis_id = thesis_data.key
+    author_id = ''
+    authors = []
+    for author_name in author_names:
+        is_exist_author = len(AUTHORS_MASTER.fetch({ 'author_name': author_name })) != 0
+        if is_exist_author:
+            author_id = AUTHORS_MASTER.fetch({ 'author_name': author_name })[0]['key']
+        else:
+            author_id = AUTHORS_MASTER.put({ 'author_name': author_name })['key']
+
+        author = AUTHORS.put({
+            'thesis_id': thesis_id,
+            'author_id': author_id
+        })
+
+        authors.append({
+            'thesis_id': author['thesis_id'],
+            'author_id': author['author_id']
+        })
+
+    thesis = THESIS.put(
         {
             'title': thesis_data.title,
             'language': thesis_data.language,
             'publish_date': thesis_data.publish_date,
             'url': thesis_data.url,
             'is_read': thesis_data.is_read,
-            'page': thesis_data.page
+            'page': thesis_data.page,
+            'authors': authors
         },
         thesis_data.key
     )
